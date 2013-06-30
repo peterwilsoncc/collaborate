7e8257718aaab6745667df8b0a90c4286b6c322b
Peter Wilson <git-commit-by@peterwilson.cc>
2013-06-30 18:49:46 +1000

    Initial commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php body_class( 'no-js' ); ?> <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title><?php wp_title( '|' ); ?></title>
+	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+	<?php wp_head(); ?>
+</head>
+<body>
