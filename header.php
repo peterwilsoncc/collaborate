bc85f58142bf267d75974bc4a5231a5294995103
Bronson Quick <bronson@sennza.com.au>
2013-07-18 19:15:05 +1000

    Add the html5 shim

--- header.php
+++ header.php
@@ -2,8 +2,15 @@
 <html <?php body_class( 'no-js' ); ?> <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width" />
 	<title><?php wp_title( '|' ); ?></title>
 	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
 	<?php wp_head(); ?>
 </head>
 <body>
