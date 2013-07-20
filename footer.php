8c01752e15b672c852f847c963743b5ada9f4dce
Peter Wilson <git-commit-by@peterwilson.cc>
2013-07-20 17:11:08 +1000

    Add footer widgets, only display if widgets are set in the admin

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+<footer class="footer foot">
+	<?php
+	if ( is_active_sidebar( 'footer-widget-area' ) ) :
+		echo '<div class="widgetized widgetized--footer">';
+		dynamic_sidebar( 'footer-widget-area' );
+		echo '</div>';
+	endif;
+	?>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
