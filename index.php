4ea61038f461d08c006add291d8c1172eb7c1a83
Peter Wilson <git-commit-by@peterwilson.cc>
2013-07-20 17:30:22 +1000

    Listing page base HTML

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php get_header(); ?>
+
+<div class="content">
+	<main role="main" class="main">
+		<!-- loop goes here -->
+	</main>
+	
+	<?php
+	if ( is_active_sidebar( 'main-sidebar' ) ) :
+		echo '<div class="widgetized widgetized--sidebar">';
+		dynamic_sidebar( 'main-sidebar' );
+		echo '</div>';
+	endif;
+	?>
+</div>
+
 <?php get_footer(); ?>
