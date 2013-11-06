73bc185d3ba3634d48cf1cccba90133f36aa237d
Peter Wilson <git-commit-by@peterwilson.cc>
2013-11-06 21:35:59 +1100

    Improve robustness of no-js/js class switch
    HTML element always picks up the js class, regardless of no-js's presence.

--- collaborate/header.php
+++ collaborate/header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width" />
 	<title><?php wp_title( '|' ); ?></title>
-	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'')+' js'})(document.documentElement)</script>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
