63544c255ce98d5d06ab323deab315124eefb568
Bronson Quick <bronson@sennza.com.au>
2014-01-29 15:03:46 +1000

    Add functions and files for BEM and HTML Tidy to show how we could tackle #8 and #9

---
+++ collaborate/inc/bem.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This file will contain all our actions and filters to intro BEM CSS naming conventions into our theme
+ */
+
+echo "BEM Support Enabled";
---
+++ collaborate/inc/html-tidy.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This file will contain all our actions and filters to clean up extra CSS classes that WordPress adds around the place
+ */
+
+echo "HTML Tidy Support Enabled";
